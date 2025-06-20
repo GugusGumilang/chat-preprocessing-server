4ded763ee4bdacad6795f7e578289193cf975b26
GugusGumilang <gugusgumilang52@gmail.com>
2025-06-20 08:46:58 +0700

    Menghapus Language tool

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, Body
 from langdetect import detect
 from spellchecker import SpellChecker
-import language_tool_python
+# import language_tool_python
 
 app = FastAPI()
-tool = language_tool_python.LanguageTool('en-US')
+# tool = language_tool_python.LanguageTool('en-US')
 spell = SpellChecker()
 
 @app.get("/")
@@ -23,11 +23,11 @@ def preprocess(input_text: str = Body(...)):
         typo_corrected = ' '.join(corrected_words)
 
         # Grammar check
-        matches = tool.check(typo_corrected)
-        grammar_issues = [m.message for m in matches]
+        # matches = tool.check(typo_corrected)
+        # grammar_issues = [m.message for m in matches]
     else:
         typo_corrected = input_text
-        grammar_issues = []
+        # grammar_issues = []
 
     # Dummy loanword correction (nanti lo tambahin logic beneran)
     loanword_corrected = typo_corrected  # sementara
@@ -37,5 +37,5 @@ def preprocess(input_text: str = Body(...)):
         "detected_lang": lang,
         "typo_corrected": typo_corrected,
         "loanword_corrected": loanword_corrected,
-        "grammar_issues": grammar_issues
+        # "grammar_issues": grammar_issues
     }
